6732cf88b149a096268e0f064cc8416666b63262
HarpalSingh7395 <harrysinghharpal58@gmail.com>
2025-09-19 12:27:22 +0530

    Updated config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,29 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    libInjectCss(),
-    dts({ include: ['lib'] }),
-    tailwindcss(),
+    dts({
+      include: ['lib'],
+    }),
   ],
   build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
-      }
-    },
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es'],
+      name: 'MentionMenu',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `main.${format}.js`,
     },
-    copyPublicDir: false
-  }
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+    copyPublicDir: false,
+  },
 })
